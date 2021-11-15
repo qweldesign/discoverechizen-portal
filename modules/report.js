f11bd5f8e22dbe82a549271e717e6ba5f3d31db4
Taigo Ito <webmaster@qwel.design>
2021-11-15 18:25:22 +0900

    Use Spreadsheet API to report reservation status

---
+++ modules/report.js
@@ -0,0 +1,74 @@
+/**
+ * Report
+ * Author: Taigo Ito (https://qwel.design/)
+ * Location: Fukui, Japan
+ */
+
+export default class Report {
+  constructor(data) {
+    this._elem = document.getElementById('report');
+    this._template = document.getElementById('reportTemplate');
+    this._renderTemplate(data);
+  }
+
+  _renderTemplate(data) {
+    data.forEach((dt) => {
+      // 表示条件
+      if (dt.info.pop().content) {
+        // テンプレートからNodeを作成
+        const item = document.importNode(this._template.content, true);
+        this._createInfoSection(item, dt.info);
+        this._createActivitySection(item, dt.activity);
+        this._createNoteSection(item, dt.note);
+        // Node挿入
+        this._elem.appendChild(item);
+      }
+    });
+  }
+
+  _createInfoSection(item, arr) {
+    const section = item.querySelector('.report__info');
+
+    // 情報タイトルと情報を順次挿入
+    const len = arr.length;
+    for (let i = 1; i < len; i++) {
+      const tr = document.createElement('tr');
+      const th = document.createElement('th');
+      th.textContent = arr[i].name;
+      tr.appendChild(th);
+      const td = document.createElement('td');
+      td.textContent = arr[i].content;
+      tr.appendChild(td);
+      section.appendChild(tr);
+    }
+  }
+
+  _createActivitySection(item, arr) {
+    const section = item.querySelector('.report__activity');
+
+    // 体験名と情報を順次挿入
+    const len = arr.length;
+    for (let i = 0; i < len; i++) {
+      const tr = document.createElement('tr');
+      const th = document.createElement('th');
+      th.textContent = arr[i].name;
+      tr.appendChild(th);
+      const td = document.createElement('td');
+      td.textContent = arr[i].content;
+      tr.appendChild(td);
+      section.appendChild(tr);
+    }
+  }
+
+  _createNoteSection(item, arr) {
+    const section = item.querySelector('.report__note');
+
+    // メモを順次挿入
+    const len = arr.length;
+    for (let i = 0; i < len; i++) {
+      const li = document.createElement('li');
+      li.textContent = arr[i];
+      section.appendChild(li);
+    }
+  }
+}
---
+++ reserve/init.js
@@ -0,0 +1,20 @@
+/**
+ * Init
+ * Author: Taigo Ito (https://qwel.design/)
+ * Location: Fukui, Japan
+ */
+
+import {ssUrl} from '../config.js';
+import Report from '../modules/report.js';
+
+const elem = document.getElementById('report');
+elem.classList.add('--loading');
+elem.textContent = 'データベースを読み込み中です。しばらくお待ちください。';
+
+(async () => {
+  const res = await fetch(ssUrl);
+  const data = await res.json();
+  elem.innerHTML = '';
+  elem.classList.remove('--loading');
+  new Report(data)
+})();
